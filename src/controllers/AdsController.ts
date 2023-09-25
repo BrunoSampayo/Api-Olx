059c6c72180a2d9e999ae30be73712e33c6112fa
Bruno Sampayo <brunosampayo1040@gmail.com>
2023-09-25 12:53:59 -0300

    Add ad method implemented

--- src/controllers/AdsController.ts
+++ src/controllers/AdsController.ts
@@ -25,14 +25,18 @@ export const getCategories = async(req:Request,res:Response)=>{
  const uploadFile = async (file: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[]; } | undefined)=>{
     if(file){
         const files= file as Express.Multer.File[]
+        console.log(files)
+        let filesPath =[]
         for(let i in files) {
             const file = files[i] ;
+           
             await sharp(file.path).resize(500).toFormat('jpeg').toFile(`./public/media/${file.filename}.jpg`);
             await unlink(file.path)
-            return file.filename;
-            
+            filesPath.push(file.filename);
             
         }
+        
+        return filesPath
         
     }else{
         new Error("Upload file error")
@@ -61,17 +65,23 @@ export const addAction = async(req:Request,res:Response) =>{
     newAd.title = title;
     newAd.category= category;
     newAd.price = price;
-    newAd.priceNegotiable = priceNegotiable(priceNegotiable==='true') ? true : false;
+    newAd.priceNegotiable = priceNegotiable==='true' ? true : false;
     newAd.description = description;
     newAd.views=0
-    //TO FINISH
+    
     if(req.files as  Express.Multer.File[] ){
        let url= await  uploadFile(req.files)
+
        if(url){
-        newAd.images.push({
-            url,
-            default:false
-        }) 
+            if(url.length > 1){
+                for(let i = 0; i < url.length; i++){
+                    i ===0 ? newAd.images.push({url:url[i],default:true}) : newAd.images.push({url:url[i],default:false})
+                     
+                }
+            }else{
+                
+                newAd.images.push({url,default:false})
+            } 
        }
     }
 
